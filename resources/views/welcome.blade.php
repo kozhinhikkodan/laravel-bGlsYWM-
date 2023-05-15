5334e4a7826461f64da208d685f1a9c8528a5977
Mohammed Salih Kozhinhikkodan <kozhinhikkodan007@gmail.com>
2023-05-15 17:07:00 +0530

    Sort added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -962,6 +962,18 @@
                             $('#results-container').html(html);
                         } else {
 
+                            users.sort(function(a, b) {
+                                var nameA = a.name.toUpperCase();
+                                var nameB = b.name.toUpperCase();
+                                if (nameA < nameB) {
+                                    return -1;
+                                }
+                                if (nameA > nameB) {
+                                    return 1;
+                                }
+                                return 0;
+                            });
+
                             var html = '';
                             for (var i = 0; i < users.length; i++) {
                                 var user = users[i];
